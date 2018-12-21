5e7b3c3e963d23d5be2a46878e63bcb0d53c4fea
Natthapong Tuscharoen <592493@egat.co.th>
2018-12-21 14:26:30 +0700

    Fix Line ID , Can Push

--- simple_bot.php
+++ simple_bot.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $channelAccessToken = 'qd9NeFW5MB99pfUIyLlpABREBMRTRrYNilFjJEb7Gqa37ipufRRdcxIR5WyFgTnVNORZVzTGzTK1n1Sr0/dRnQ8inCdcRRGET3nteJre8zdNiil0dvjXvvUK3r/xUcE0NkRUEBE8PGABs5syn03xLQdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '1d596833968f8bce1da3bfeac10912d3';
-$boss_lineID = "Udfdbe68286f0b0614403c2322def21d";
+$boss_lineID = "Udfdbe68286f0b0614403c2322def21d3";
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($channelAccessToken);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
@@ -21,7 +21,7 @@ echo "fail";
 }
  */
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('Hello is Time is :' . date("d-m-Y H:i:s"));
 $response = $bot->pushMessage($boss_lineID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
