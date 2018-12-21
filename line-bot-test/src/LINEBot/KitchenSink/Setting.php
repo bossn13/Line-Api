80ddbdaf44883916b9e9e5ded1442a77d23ba110
Natthapong Tuscharoen <592493@egat.co.th>
2018-12-21 13:28:39 +0700

    Add Full test

---
+++ line-bot-test/src/LINEBot/KitchenSink/Setting.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+namespace LINE\LINEBot\KitchenSink;
+
+class Setting
+{
+    public static function getSetting()
+    {
+        return [
+            'settings' => [
+                'displayErrorDetails' => true, // set to false in production
+
+                'logger' => [
+                    'name' => 'slim-app',
+                    'path' => __DIR__ . '/../../../logs/app.log',
+                ],
+
+                'bot' => [
+                    'channelToken' => getenv('LINEBOT_CHANNEL_TOKEN') ?: 'qd9NeFW5MB99pfUIyLlpABREBMRTRrYNilFjJEb7Gqa37ipufRRdcxIR5WyFgTnVNORZVzTGzTK1n1Sr0/dRnQ8inCdcRRGET3nteJre8zdNiil0dvjXvvUK3r/xUcE0NkRUEBE8PGABs5syn03xLQdB04t89/1O/w1cDnyilFU=',
+                    'channelSecret' => getenv('LINEBOT_CHANNEL_SECRET') ?: '1d596833968f8bce1da3bfeac10912d3',
+                ],
+
+                'apiEndpointBase' => getenv('LINEBOT_API_ENDPOINT_BASE'),
+            ],
+        ];
+    }
+}
